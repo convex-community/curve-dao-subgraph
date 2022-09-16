31a8d2047e8db872c8023738877084c57f6fdd82
benber86 <benoitb@tutanota.de>
2022-09-16 22:18:41 +1000

    Fix error with non nullable field on gauge type

--- src/services/gauges.ts
+++ src/services/gauges.ts
@@ -1,9 +1,10 @@
 import { GaugeType } from '../../generated/schema'
-import { BigInt } from '@graphprotocol/graph-ts'
+import { BigDecimal, BigInt } from '@graphprotocol/graph-ts'
 
 export function registerGaugeType(id: string, name: string): GaugeType {
   const gaugeType = new GaugeType(id)
   gaugeType.name = name
   gaugeType.gaugeCount = BigInt.zero()
+  gaugeType.weight = BigDecimal.zero()
   return gaugeType
 }
